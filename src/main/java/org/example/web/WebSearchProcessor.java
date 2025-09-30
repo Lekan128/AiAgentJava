a41330847ba836acb33c7a4a975fc8985777ad72
Olalekan Amoo <olalekanamoo128@gmail.com>
2025-09-30 09:40:20 +0100

    Make WebSearchProcessor return List<WebsiteTextExtractor.ExtractedPageSummary>

--- src/main/java/org/example/web/WebSearchProcessor.java
+++ src/main/java/org/example/web/WebSearchProcessor.java
@@ -1,6 +1,7 @@
 package org.example.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.ObjectMapperSingleton;
 import org.example.method.AiToolMethod;
 import org.example.method.description.ArgDesc;
 import org.example.web.search.Google;
@@ -13,17 +14,20 @@ import java.util.List;
 public class WebSearchProcessor {
 
     @AiToolMethod("Search the web for information")
-    public static String search(@ArgDesc("The search parameter") String searchParam) throws IOException, InterruptedException {
+    public static List<WebsiteTextExtractor.ExtractedPageSummary> search(@ArgDesc("The search parameter") String searchParam) throws IOException, InterruptedException {
         List<GoogleSearchResponse> searchResponses = Google.search(searchParam);
         //Using only the first 2 search results.
         if (searchResponses.size() >2) searchResponses = searchResponses.subList(0, 2);
         List<WebsiteTextExtractor.ExtractedPageSummary> extractedPages = WebsiteTextExtractor.extractAllNonNullSearchResponseSummary(searchResponses);
-        return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(extractedPages);
+        return extractedPages;
+//        return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(extractedPages);
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(search("Jenga"));
+            List<WebsiteTextExtractor.ExtractedPageSummary> search = search("Dr. Rashel Vitamin C Brightening & Anti-Aging Face Cream");
+            String s = ObjectMapperSingleton.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(search);
+            System.out.println(s);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
